376ea8d9b8e4db6efcdcd5a9cd4cc525b84a4831
Anders Cedronius <anders.cedronius@icloud.com>
2021-01-03 19:31:15 +0100

    Minor update

--- install_msdk1.sh
+++ install_msdk1.sh
@@ -6,25 +6,27 @@ set -e
 sudo apt -y update && sudo apt -y upgrade 
 sudo apt install -y gdb build-essential cmake autoconf libtool libdrm-dev pkg-config libpciaccess-dev xutils-dev libx11-dev xorg-dev
 
+MSDKBASE=$(pwd)
+
 # libva
 echo "**************************************"
 echo "*** Building and installing libva  ***"
 echo "**************************************"
-rm -rf ~/vaapi && mkdir ~/vaapi && cp *.* ~/vaapi && cd ~/vaapi
 #wget https://github.com/intel/libva/archive/2.10.0.tar.gz
+rm -rf libva-2.10.0
 tar -xvf 2.10.0.tar.gz
 cd libva-2.10.0/
 ./autogen.sh --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu
 time make -j$(nproc)
 sudo make -j$(nproc) install
-rm 2.10.0.tar.gz
 
 # libva-utils
 echo "********************************************"
 echo "*** Building and installing libva-utils  ***"
 echo "********************************************"
-cd ~/vaapi
+cd $MSDKBASE
 #wget https://github.com/intel/libva-utils/archive/2.10.0.tar.gz
+rm -rf libva-utils-2.10.0
 tar -xvf tool-2.10.0.tar.gz
 cd libva-utils-2.10.0/
 ./autogen.sh 
@@ -35,8 +37,9 @@ sudo make -j$(nproc) install
 echo "***************************************"
 echo "*** Building and installing gemlib  ***"
 echo "***************************************"
-cd ~/vaapi
+cd $MSDKBASE
 #wget https://github.com/intel/gmmlib/archive/intel-gmmlib-20.4.1.tar.gz
+rm -rf gmmlib-intel-gmmlib-20.4.1
 tar -xvf intel-gmmlib-20.4.1.tar.gz
 cd gmmlib-intel-gmmlib-20.4.1/
 mkdir -p build_gmm && cd build_gmm
@@ -48,8 +51,9 @@ sudo make -j$(nproc) install
 echo "*********************************************"
 echo "*** Building and installing media-driver  ***"
 echo "*********************************************"
-cd ~/vaapi
+cd $MSDKBASE
 #wget https://github.com/intel/media-driver/archive/intel-media-20.4.5.tar.gz
+rm -rf media-driver-intel-media-20.4.5/
 tar -xvf intel-media-20.4.5.tar.gz
 cd media-driver-intel-media-20.4.5/
 mkdir -p build_md && cd build_md
--- install_msdk2.sh
+++ install_msdk2.sh
@@ -7,16 +7,15 @@ set -e
 echo "******************************************"
 echo "*** Building and installing Media SDK  ***"
 echo "******************************************"
-
+MSDKBASE=$(pwd)
 sudo echo #to avoid the break at sudo make install
-cd ~/vaapi
 #wget https://github.com/Intel-Media-SDK/MediaSDK/archive/intel-mediasdk-20.5.1.tar.gz
+rm -rf MediaSDK-intel-mediasdk-20.5.1/
 tar -xvf intel-mediasdk-20.5.1.tar.gz
 cd MediaSDK-intel-mediasdk-20.5.1/
 mkdir build && cd build
 cmake -DBUILD_TESTS=ON -DBUILD_TOOLS=ON ..
 time make -j$(nproc)
 sudo make -j$(nproc) install
-cd ~
-rm -rf ~/vaapi
+cd $MSDKBASE
 
