f0b77857788c324d2f8e0a85571a71d5b0bafa5c
Tomas Öhberg <tomas.ohberg@edgeware.tv>
2021-04-08 16:58:19 +0200

    Fix copying of cttmetrics.h file

--- install_msdk2.sh
+++ install_msdk2.sh
@@ -13,11 +13,13 @@ sudo echo #to avoid the break at sudo make install
 rm -rf MediaSDK-intel-mediasdk-21.1.3/
 tar -xvf intel-mediasdk-21.1.3.tar.gz
 cd MediaSDK-intel-mediasdk-21.1.3/
-mkdir build && cd build
+mkdir build
+cd build
 cmake -DBUILD_TESTS=ON -DBUILD_TOOLS=ON ..
 time make -j$(nproc)
 sudo make -j$(nproc) install
 # Copy a helper header used for getting GPU metrics. The corresponding libcttmetrics.so is already located in /opt/intel/mediasdk/share/mfx/samples/
+cd ..
 sudo cp samples/metrics_monitor/include/cttmetrics.h /opt/intel/mediasdk/include/mfx/
 cd $MSDKBASE
 
